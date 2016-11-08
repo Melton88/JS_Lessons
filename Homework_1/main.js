1d5a120a8808cf6575db83d153e25fb1cee09f64
Melton <melton88@live.ru>
2016-11-08 21:36:47 +0300

    Добавлены тексты домашних заданий

--- Homework_1/main.js
+++ Homework_1/main.js
@@ -1,6 +1,13 @@
 /**
  * Created by melton on 08.10.2016.
+  ●	Вывести «Привет, Javascript» c помощью alert
+ ●	Работа с переменными
+ ●	Объявите две переменные: admin и name.
+ ●	Запишите в name строку "Василий".
+ ●	Скопируйте значение из name в admin.
+ ●	Выведите admin (должно вывести «Василий»).
  */
+
 "use strict";
 alert('Привет, JavaScript!');
 var admin, name='Василий';
